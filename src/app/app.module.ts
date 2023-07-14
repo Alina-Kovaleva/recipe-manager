e67c1486e30161626b64e7e1f5b15120490c34b4
Alina Kovaleva <calipco.xp@gmail.com>
2023-07-14 15:14:17 +0200

    Initialized project structure with basic component placeholders
    - HeaderComponent: Created for application header management.
    - RecipesComponent: Main component for recipe display.
    - RecipeListComponent: Displays a list of available recipes.
    - RecipeItemComponent: Represents an individual item in the recipe list.
    - RecipeDetailComponent: Shows detailed information for a selected recipe.
    - ShoppingListComponent: Handles the shopping list feature, listing items for purchase.
    - ShoppingListEditComponent: Enables adding and editing of items in the shopping list.
    - FooterComponent: Manages the application's footer.
    
    Note: Currently, all these components are placeholders and do not include any functionality.
    They represent the project's structure.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/shared/components/header/header.component';
+import { RecipesComponent } from './core/shared/components/recipes/recipes.component';
+import { RecipeListComponent } from './core/shared/components/recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './core/shared/components/recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './core/shared/components/recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './core/shared/components/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './core/shared/components/shopping-list/shopping-edit/shopping-edit.component';
+import { FooterComponent } from './core/shared/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
